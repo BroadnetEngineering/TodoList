d94db076c80b43cb64fcbc6b0edadf6bc88f7992
jonathanhovland <jonathan.hovland@yahoo.com>
2020-06-04 07:58:59 -0600

    Complete status and remove functionality are working correctly

--- todo-list/src/components/TodoList.js
+++ todo-list/src/components/TodoList.js
@@ -1,17 +1,19 @@
 import React from "react";
 import TodoTask from "./TodoTask";
 
-function TodoList({ todos }) {
+function TodoList({ todos, toggleStatus, removeTodo }) {
     return (
         <ul>
             {todos.map(todo => (
                 <TodoTask
-                    key={ todo.id }
-                    todo={ todo }
+                    key = { todo.id }
+                    todo = { todo }
+                    toggleStatus = { toggleStatus }
+                    removeTodo = { removeTodo }
                 />
             ))}
         </ul>
-    )
+    );
 }
 
 export default TodoList;
--- todo-list/src/components/TodoTask.js
+++ todo-list/src/components/TodoTask.js
@@ -1,11 +1,29 @@
 import React from "react";
 
-function TodoTask({ todo }) {
+function TodoTask({ todo, toggleStatus, removeTodo }) {
+    
+    function handleStatusClick() {
+        toggleStatus(todo.id)
+    }
+
+    function handleRemoveClick() {
+        removeTodo(todo.id)
+    }
+
     return (
         <React.Fragment style={{ display: "flex" }}>
-            <input type="checkbox" />
-            <li>{ todo.task }</li>
-            <button>X</button>
+            <input 
+                type="checkbox"
+                onClick={ handleStatusClick } 
+            />
+            <li
+                style={{
+                    textDecoration: todo.completed ? "line-through" : null
+                }}
+            >
+                { todo.task }
+            </li>
+            <button onClick={ handleRemoveClick }>Delete</button>
         </React.Fragment>
     )
 }
